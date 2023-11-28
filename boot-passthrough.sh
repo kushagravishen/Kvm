a2c9cca557fc1233f61cdb618075912bd53e57b8
Kushagra Vishen <80204384+kushagravishen@users.noreply.github.com>
2023-11-28 10:41:32 +0530

    Update boot-passthrough.sh

--- boot-passthrough.sh
+++ boot-passthrough.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Special thanks to:
-# https://github.com/Leoyzen/KVM-Opencore
-# https://github.com/thenickdude/KVM-Opencore/
-# https://github.com/qemu/qemu/blob/master/docs/usb2.txt
-#
 # qemu-img create -f qcow2 mac_hdd_ng.img 128G
 #
 # echo 1 > /sys/module/kvm/parameters/ignore_msrs (this is required)
